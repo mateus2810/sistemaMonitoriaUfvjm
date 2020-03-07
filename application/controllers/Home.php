5b6606faf578d670f1c05533190fa0a43291ed18
Alex Lopes Da Rocha <alex.rocha@ufvjm.edu.br>
2020-03-07 01:39:26 +0000

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -95,7 +95,7 @@ class Home extends CI_Controller
 
         //recupera os dados do formulario
         $matricula = $this->input->post('matricula');
-        $senha = md5($this->input->post('senha'));
+        $senha = $this->input->post('senha');
         //$senha = $this->input->post('senha');
         //$password = md5($password);
 
