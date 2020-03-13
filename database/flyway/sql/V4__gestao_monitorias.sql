a3566a27b85c5caa86edca094017ba4ec11e3986
Mateus Amaral <mateus.curso123@gmail.com>
2020-03-13 14:47:06 -0300

    apagar

--- database/flyway/sql/V4__gestao_monitorias.sql
+++
@@ -1,7 +0,0 @@
--- -----------------------------------------------------
--- Table `gestao_monitorias`.`usuario`
--- -----------------------------------------------------
-ALTER TABLE usuario
-ADD declaracao boolean;
-ADD status_prof boolean;
-
