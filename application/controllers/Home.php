7bf02dca7a0e8cd4fd632e1b91329f9166d4ab19
Everton Paiva <evertonpaiva@gmail.com>
2020-06-08 06:52:13 -0300

    Atualizando maneira de realizar login.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -111,15 +111,15 @@ class Home extends CI_Controller
             $request->containstitucional = $containstitucional;
             $request->password = $senha;
 
+            // Carrega a classe de autenticação
             $authGraphqlRequest = new AuthGraphqlRequest();
-            // Tenta realizar o login na Conta Institucional
-            $headers = $authGraphqlRequest->loginContaInstitucional($request);
 
-            // Login ok, recria a conexao com o GraphQL, agora com cabeçalhos de autenticação
-            $loggedAuthGraphqlRequest = new AuthGraphqlRequest($headers);
+            // Tenta realizar o login na Conta Institucional
+            $authGraphqlRequest->loginContaInstitucional($request);
 
             // Recupera as informações do usuário logado
-            $usuarioLogadoInfo = $loggedAuthGraphqlRequest->usuarioLogadoInfo();
+            // Dados pessoais e vinculos (aluno, docente, tae, coordenador de curso, etc) com a UFVJM
+            $usuarioLogadoInfo = $authGraphqlRequest->usuarioLogadoInfo();
 
             $DATA = $this->Usuario_model->getUsuarioByContaInstitucional($containstitucional);
 
