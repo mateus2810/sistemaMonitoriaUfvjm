0b43bc85a84f02ba71959db8d45ac22e7af08e17
Alex Lopes <alexlr10@hotmail.com>
2020-07-06 15:02:28 -0300

    modificando quantidade de caracteres de dependencias

--- database/flyway/sql/V6__gestao_monitorias.sql
+++ database/flyway/sql/V6__gestao_monitorias.sql
@@ -1,6 +1,6 @@
 -- Table `gestao_monitorias`.`local`
 -- -----------------------------------------------------
 ALTER TABLE local
-ADD dependencia varchar(50),
+ADD dependencia varchar(70),
 DROP COLUMN predio,
 DROP COLUMN sala;
